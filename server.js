450d25198b63b78c84ef24cab1cdff9656701b73
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-13 04:02:25 -0400

    Initial upload

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const taskRoutes = require('./routes/task')
+
+require('dotenv').config({ path: './config/.env' })
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      client: mongoose.connection.getClient()
+    }),
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
+app.use('/', mainRoutes)
+app.use('/task', taskRoutes)
+
+app.listen(process.env.PORT || 2121, () => {
+  console.log('Server is running, you better catch it!')
+})    
