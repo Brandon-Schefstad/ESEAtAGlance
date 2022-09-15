8bb3cb109eb7bd6b793391b8369ce9ebea2451a8
Brandon-Schefstad <bschefstad@gmail.com>
2022-09-15 15:02:28 -0400

    Feat: Can search for specific grade history

--- public/js/student.js
+++ public/js/student.js
@@ -1,9 +1,19 @@
 const goalSearch = document.querySelector('.goalSearch');
 const goalForm = document.querySelector('.goalForm');
+const ID = document.querySelector('.ID');
 goalSearch.addEventListener('click', findGoals);
 
 async function findGoals() {
-	alert('works');
+	console.log(ID.innerText.split(':')[1]);
 	console.log(goalForm);
+	const goalArray = Object.values(goalForm);
+	const goalInputArray = goalArray.filter((goal) => {
+		return goal.checked;
+	});
+	const goalSearchArray = goalInputArray.map((goal) => {
+		return goal.value;
+	});
+	console.log(goalSearchArray);
+
 	let searchArr = [];
 }
