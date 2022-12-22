74a00f9c7e2e9786dbb530bf38dba8b1cb70a2fe
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-22 15:49:18 -0500

    Updating process.env

--- controllers/student/searchStudent.js
+++ controllers/student/searchStudent.js
@@ -2,16 +2,16 @@ const populateStudentResObject = require('../utils/populateStudentResObject');
 const cloudinary = require('../../middleware/cloudinary');
 const dashboard = require('../dashboard.js');
 const { ObjectId } = require('mongodb');
+const Student = require('../../models/Student');
 
 module.exports = {
-	searchStudent: async (req, res) => {
-		try {
-			const resObject = await populateStudentResObject(req.query.ID);
-			console.log(resObject);
-			res.cookie('ID', `${req.query.ID}`, { httpOnly: true });
-			res.render('searchStudent', { data: resObject });
-		} catch (error) {
-			res.render('searchStudent');
-		}
-	},
+  searchStudent: async (req, res) => {
+    try {
+      const resObject = await populateStudentResObject(req.query.ID);
+      res.cookie('ID', `${req.query.ID}`, { httpOnly: true });
+      res.render('searchStudent', { data: resObject });
+    } catch (error) {
+      res.render('searchStudent');
+    }
+  },
 };
