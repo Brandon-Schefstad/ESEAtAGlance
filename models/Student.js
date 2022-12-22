92c7c90e3b751ee416ae55c8269eb385db1ded11
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-22 15:50:25 -0500

    Removing circular dependent schemas

--- models/Student.js
+++ models/Student.js
@@ -1,57 +1,55 @@
 const mongoose = require('mongoose');
-const TeacherSchema = require('./Teacher.js').schema;
-const GoalSchema = require('./Goals.js').schema;
 
 const StudentSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
-		required: true,
-	},
-	lastName: {
-		type: String,
-		required: true,
-	},
-	ID: {
-		type: Number,
-	},
-	grade: {
-		type: Number,
-	},
-	primaryExceptionality: {
-		type: String,
-		required: true,
-	},
-	secondaryExceptionality: {
-		type: String,
-	},
-	caseManager: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Teacher',
-	},
-	history: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Goals',
-		},
-	],
-	accommodations: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Accommodations',
-		},
-	],
-	IEPDueDate: {
-		type: Date,
-		required: true,
-	},
-	cloudinaryID: {
-		type: String,
-		required: true,
-	},
-	image: {
-		type: String,
-		required: true,
-	},
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  ID: {
+    type: Number,
+  },
+  grade: {
+    type: Number,
+  },
+  primaryExceptionality: {
+    type: String,
+    required: true,
+  },
+  secondaryExceptionality: {
+    type: String,
+  },
+  caseManager: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Teacher',
+  },
+  history: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Goals',
+    },
+  ],
+  accommodations: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Accommodations',
+    },
+  ],
+  IEPDueDate: {
+    type: Date,
+    required: true,
+  },
+  cloudinaryID: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Student', StudentSchema);
