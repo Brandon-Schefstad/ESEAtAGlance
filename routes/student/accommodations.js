fb12d6f257da8ed08e0ecf198857c4b62cc79853
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-23 20:33:43 -0500

    Cookies now erase on searchStudent page

--- routes/student/accommodations.js
+++ routes/student/accommodations.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const accommodationsController = require('../../controllers/student/accommodations.js');
-const { ensureAuth } = require('../../middleware/auth');
+const express = require('express')
+const router = express.Router()
+const accommodationsController = require('../../controllers/student/accommodations.js')
+const { ensureAuth } = require('../../middleware/auth')
 
 router.get(
 	'/addAccommodations',
 	ensureAuth,
 	accommodationsController.addAccommodations
-);
+)
 router.get(
 	'/addAccommodations/:id',
 	ensureAuth,
 	accommodationsController.addAccommodationsLoaded
-);
+)
 router.post(
 	'/addAccommodations',
 	ensureAuth,
 	accommodationsController.postAccommodations
-);
+)
 
 router.get(
-	'/loadAccommodations',
+	'/getAccommodationsPage',
 	ensureAuth,
-	accommodationsController.loadAccommodations
-);
-module.exports = router;
+	accommodationsController.getAccommodationsPage
+)
+module.exports = router
