54ea8a7e5ede7aa0ef0854fe46283b061ff78242
Brandon-Schefstad <bschefstad@gmail.com>
2023-01-04 08:39:32 -0500

    Server: Removed images for now

--- server/routes/student/addNewStudent.js
+++ server/routes/student/addNewStudent.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const addNewStudentController = require('../../controllers/student/addNewStudent');
-const { ensureAuth } = require('../../middleware/auth');
-const upload = require('../../middleware/multer');
+const express = require('express')
+const router = express.Router()
+const addNewStudentController = require('../../controllers/student/addNewStudent')
+const { ensureAuth } = require('../../middleware/auth')
+const upload = require('../../middleware/multer')
 
-router.get(
-	'/addNewStudent',
-	ensureAuth,
-	addNewStudentController.getAddNewStudentPage
-);
 router.post(
 	'/addNewStudent',
 	ensureAuth,
-	upload.single('file'),
+	// upload.single('profileImg'),
 	addNewStudentController.postNewStudent
-);
-module.exports = router;
+)
+module.exports = router
