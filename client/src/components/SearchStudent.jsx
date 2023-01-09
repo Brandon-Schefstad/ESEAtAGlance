d442ebff571b5cf37496064ac584bfbccdef737d
Brandon-Schefstad <bschefstad@gmail.com>
2023-01-09 05:59:34 -0500

    Client: Search by ID

--- client/src/components/SearchStudent.jsx
+++ client/src/components/SearchStudent.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
-
 const AddNewStudent = () => {
+	const [student, setStudent] = useState()
+	const [studentIdToSend, setStudentIdToSend] = useState()
+	async function searchStudent(e) {
+		e.preventDefault()
+		const { data, status } = axios.get(
+			`/student/searchStudent/${studentIdToSend}`
+		)
+		console.log(status)
+		console.log(data)
+	}
 	return (
 		<>
 			<Navbar />
 			<div>Search</div>
+			<form onSubmit={searchStudent}>
+				<label
+					htmlFor="studentId"
+					onChange={(e) => setStudentIdToSend(e.target.value)}>
+					Enter Student Id: <input type="number" />
+				</label>
+			</form>
 		</>
 	)
 }
