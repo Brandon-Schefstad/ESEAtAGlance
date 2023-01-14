32e3239d9c4d67c16c895952e60654c389a580e3
Brandon-Schefstad <bschefstad@gmail.com>
2023-01-14 00:36:56 -0500

    Pushing for cyclic

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(logger('dev'))
 app.use(cookieParser())
 app.use(methodOverride('_method'))
 app.use(flash())
-//
+
 app.use(cors())
 app.use('/api/student', studentRoutes)
 app.use('/api/dashboard', dashboardRoutes)
